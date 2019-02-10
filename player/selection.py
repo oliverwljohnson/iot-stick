72227d2d72619a2bae8c86e32036052de66d7d5e
Oliver Johnson DELLXPS <u6406755@anu.edu.au>
2019-02-10 15:14:14 +1100

    rudimentary suggestion, looks at one track

--- player/selection.py
+++ player/selection.py
@@ -7,8 +7,7 @@ Created on Sat Feb  9 21:04:42 2019
 
 import spotipy
 import spotipy.util as util
-import time
-import math
+import json
 
 SPOTIPY_CLIENT_ID ='2722912b2a164140bc1ba1919913f5cd'
 SPOTIPY_CLIENT_SECRET ='49929317ad3e4d0a87624f29525acd43'
@@ -30,13 +29,19 @@ else:
 # resuts = client.Spotify.current_user_saved_tracks(spotify)
 # spotify.trace = False
 def getSuggestions():
-    networkStateFile = open("reccomendations", "w")
-    return True
+    reccomendFile = open("reccomendations", "r")
+    genres = ["rock"]
+    print("Of the Availiable genres:", spotify.recommendation_genre_seeds(), "\n the following seeds were given:", genres)
+    # TODO: Implement the file format and thus parsing code
+    reccomendationsObject = spotify.recommendations(seed_genres=genres,limit=1,country="AU")
+    return reccomendationsObject
 
 def playSuggestion():
     return True
 
-getSuggestions()
+suggested = getSuggestions()
+print(json.dumps(suggested,indent=3))
+print(suggested["tracks"][0]["name"])
 
 
 
