206b0250a354ae2f1508fa6e7aff4489e178ff51
Oliver Johnson DELLXPS <u6406755@anu.edu.au>
2019-02-10 13:52:23 +1100

    experiment concluded, after re-write selection script written in python

--- controller/api-docs.md
+++
@@ -1,9 +0,0 @@
-each request goes:
-(endpoint-path)-method-headers-data
-
-for spotify
-https://api.spotify.com
-
-### query parameters
-They always begin with a question mark (?). Each parameter pair is then separated with an ampersand (&), like this:
-
--- player/selection.py
+++ player/selection.py
@@ -13,25 +13,28 @@ SPOTIPY_CLIENT_ID ='2722912b2a164140bc1ba1919913f5cd'
 SPOTIPY_CLIENT_SECRET ='49929317ad3e4d0a87624f29525acd43'
 SPOTIPY_REDIRECT_URI ='http://localhost:8888'
 REFRESH_TOKEN = 'AQCzdqLIGDRJKyZdGybmxW2Ba9tg1OE0SL1gvLRHwNTxwdzFmybQt3rc8jB-NmplleDs583k2BmwoH2NwvMTtHC_gev54D6a0tB7kK6Sl9LCW4G7NeezXjCNpPvP6NEK-bgiLw'
-scopes = 'user-read-private user-read-email playlist-modify-private playlist-modify-public playlist-read-collaborative user-modify-playback-state user-read-currently-playing user-read-playback-state user-top-read user-read-recently-played app-remote-control streaming user-read-email user-follow-read user-follow-modify user-library-modify user-library-read'
-user = "Oliver.Johnson1998@gmail.com"
+scope = 'user-read-private user-read-email playlist-modify-private playlist-modify-public playlist-read-collaborative user-modify-playback-state user-read-currently-playing user-read-playback-state user-top-read user-read-recently-played app-remote-control streaming user-read-email user-follow-read user-follow-modify user-library-modify user-library-read'
+username = "Oliver.Johnson1998@gmail.com"
 spotify = spotipy.Spotify
 
 
-def initalise(id,secret,redirect,scope,username):
-    token = util.prompt_for_user_token(username,scope,client_id=id,client_secret=secret,redirect_uri=redirect)
-    if token:
-        spotify = spotipy.Spotify(auth=token)
-        print("Initialization Complete, auth approved for", username, "for scopes:", scope)
+token = util.prompt_for_user_token(username,scope,client_id=SPOTIPY_CLIENT_ID,client_secret=SPOTIPY_CLIENT_SECRET,redirect_uri=SPOTIPY_REDIRECT_URI)
+if token:
+    spotify = spotipy.Spotify(auth=token)
+    print("Initialization Complete, auth approved for", username, "for scopes:", scope)
+else:
+    print("Auth failed for", username)
 
-    else:
-        print("Auth failed for", username)
 
-initalise(SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI,scopes,user)
 # resuts = client.Spotify.current_user_saved_tracks(spotify)
-spotify.trace = False
-results = spotify.current_user_saved_tracks(limit=1)
-print(results)
+# spotify.trace = False
+
+
+# Read the options from file
+# Get suggestions
+# Check suggestions against songs played for the last 2 hours
+#   If song is new then play
+#   else get reccomendations changing
 
 
 
