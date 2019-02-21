69bdbc9101e044e460d6490b1fa737b23373f829
Oliver Johnson DELLXPS <u6406755@anu.edu.au>
2019-02-22 04:30:50 +1100

    replaced BLE with ESP-Mesh

---
+++ mesh/get-started/main/get_started.c
@@ -0,0 +1,429 @@
+/*
+ * ESPRESSIF MIT License
+ *
+ * Copyright (c) 2018 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
+ *
+ * Permission is hereby granted for use on all ESPRESSIF SYSTEMS products, in which case,
+ * it is free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "mdf_common.h"
+#include "mwifi.h"
+
+/* UART asynchronous example, that uses separate RX and TX tasks
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "soc/uart_struct.h"
+#include "string.h"
+
+/*
+   ANALOG READER
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+// #define MEMORY_DEBUG
+
+/* ADC1 Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+static const char *TAG = "mesh_node";
+#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES   64          //Multisampling
+#define NO_OF_GENRES    7
+
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_channel_t channel = ADC_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static const adc_unit_t unit = ADC_UNIT_2;
+static uint32_t genre;
+    mwifi_config_t config   = {
+        .channel   = CONFIG_MESH_CHANNEL,
+        .mesh_id   = CONFIG_MESH_ID,
+        .mesh_type = CONFIG_DEVICE_TYPE,
+    };
+
+
+static void check_efuse()
+{
+    //Check TP is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        printf("eFuse Two Point: Supported\n");
+    } else {
+        printf("eFuse Two Point: NOT supported\n");
+    }
+
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
+        printf("eFuse Vref: Supported\n");
+    } else {
+        printf("eFuse Vref: NOT supported\n");
+    }
+}
+
+static void print_char_val_type(esp_adc_cal_value_t val_type)
+{
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        printf("Characterized using Two Point Value\n");
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        printf("Characterized using eFuse Vref\n");
+    } else {
+        printf("Characterized using Default Vref\n");
+    }
+}
+
+// Use this function to get genre from the pin on the board
+static uint32_t getGenre()
+{
+    
+     //Check if Two Point or Vref are burned into eFuse
+    check_efuse();
+
+    //Configure ADC
+    if (unit == ADC_UNIT_1) {
+        adc1_config_width(ADC_WIDTH_BIT_12);
+        adc1_config_channel_atten(channel, atten);
+    } else {
+        adc2_config_channel_atten((adc2_channel_t)channel, atten);
+    }
+
+    //Characterize ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
+
+    //Continuously sample ADC1
+    uint32_t adc_reading = 0;
+    adc_reading = 0;
+    //Multisampling
+    for (int i = 0; i < NO_OF_SAMPLES; i++) {
+        if (unit == ADC_UNIT_1) {
+            adc_reading += adc1_get_raw((adc1_channel_t)channel);
+        } else {
+            int raw;
+            adc2_get_raw((adc2_channel_t)channel, ADC_WIDTH_BIT_12, &raw);
+            adc_reading += raw;
+        }
+    }
+    adc_reading /= NO_OF_SAMPLES;
+    //Convert adc_reading to voltage in mV
+    uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+    printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
+    //vTaskDelay(pdMS_TO_TICKS(1000));
+    return adc_reading /= (4095 / NO_OF_GENRES);
+}
+
+/*
+*  UART FUNCTIONS
+*
+*
+*/
+static const int RX_BUF_SIZE = 1024;
+
+#define TXD_PIN (GPIO_NUM_4)
+#define RXD_PIN (GPIO_NUM_5)
+
+void init() {
+    const uart_config_t uart_config = {
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+}
+
+int sendData(const char* logName, const char* data)
+{
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
+    return txBytes;
+}
+
+static void root_task(void *arg)
+{
+    mdf_err_t ret                    = MDF_OK;
+    char* data                       = MDF_MALLOC(MWIFI_PAYLOAD_LEN); // Allocates space in memory and gives it a pointer to first byte
+    size_t size                      = MWIFI_PAYLOAD_LEN;
+    uint8_t src_addr[MWIFI_ADDR_LEN] = {0x0};
+    mwifi_data_type_t data_type      = {0}; // Sets an array type
+    
+    //MDF_LOGI("The data looks like this:")
+
+    MDF_LOGI("Root is running");
+    init();
+
+    for (int i = 0;; ++i) {
+        if (!mwifi_is_started()) {
+            vTaskDelay(500 / portTICK_RATE_MS);
+            continue;
+        }
+
+        size = MWIFI_PAYLOAD_LEN;
+        memset(data, 0, MWIFI_PAYLOAD_LEN); // assign memory for the payload
+        
+        ret = mwifi_root_read(src_addr, &data_type, data, &size, portMAX_DELAY);
+        MDF_ERROR_CONTINUE(ret != MDF_OK, "<%s> mwifi_root_read", mdf_err_to_name(ret));
+        MDF_LOGI("Root receive, addr: " MACSTR ", size: %d, data: %s", MAC2STR(src_addr), size, data);
+
+        static const char *TX_TASK_TAG = "TX_TASK";
+        esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+        ESP_LOGI(TX_TASK_TAG, "Sending preferences...");
+        // Output prefs as enum e.g "0"
+        sendData(TX_TASK_TAG,data);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+        //uint32_t genre = getGenre();
+        size = sprintf(data, "Root Received Message");
+        ret = mwifi_root_write(src_addr, 1, &data_type, data, size, true);
+        MDF_ERROR_CONTINUE(ret != MDF_OK, "mwifi_root_recv, ret: %x", ret);
+        MDF_LOGI("Root send, addr: " MACSTR ", size: %d, data: %s", MAC2STR(src_addr), size, data);
+    }
+
+    MDF_LOGW("Root is exit");
+    MDF_FREE(data);
+    vTaskDelete(NULL);
+}
+
+static void node_read_task(void *arg)
+{
+    mdf_err_t ret = MDF_OK;
+    char *data    = MDF_MALLOC(MWIFI_PAYLOAD_LEN);
+    size_t size   = MWIFI_PAYLOAD_LEN;
+    mwifi_data_type_t data_type      = {0x0};
+    uint8_t src_addr[MWIFI_ADDR_LEN] = {0x0};
+
+    MDF_LOGI("Note read task is running");
+
+    for (;;) {
+        if (!mwifi_is_connected()) {
+            vTaskDelay(500 / portTICK_RATE_MS);
+            continue;
+        }
+
+        size = MWIFI_PAYLOAD_LEN;
+        memset(data, 0, MWIFI_PAYLOAD_LEN);
+        ret = mwifi_read(src_addr, &data_type, data, &size, portMAX_DELAY);
+        MDF_ERROR_CONTINUE(ret != MDF_OK, "mwifi_read, ret: %x", ret);
+        MDF_LOGI("Node receive, addr: " MACSTR ", size: %d, data: %s", MAC2STR(src_addr), size, data);
+    }
+
+    MDF_LOGW("Note read task is exit");
+
+    MDF_FREE(data);
+    vTaskDelete(NULL);
+}
+
+void node_write_task(void *arg)
+{
+    mdf_err_t ret = MDF_OK;
+    int count     = 0;
+    size_t size   = 0;
+    char *data    = MDF_MALLOC(MWIFI_PAYLOAD_LEN);
+    mwifi_data_type_t data_type = {0x0};
+
+    MDF_LOGI("Node write task is running");
+
+    for (;;) {
+        if (!mwifi_is_connected()) {
+            vTaskDelay(500 / portTICK_RATE_MS);
+            continue;
+        }
+        
+        //int genre = getGenre();
+        MDF_LOGI("Node sending genre... %d", genre);
+        size = sprintf(data, "%d", genre);
+        ret = mwifi_write(NULL, &data_type, data, size, true);
+        MDF_ERROR_CONTINUE(ret != MDF_OK, "mwifi_write, ret: %x", ret);
+
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+
+    MDF_LOGW("Node write task is exit");
+
+    MDF_FREE(data);
+    vTaskDelete(NULL);
+}
+
+/**
+ * @brief Timed printing system information
+ */
+static void print_system_info_timercb(void *timer)
+{
+    uint8_t primary                 = 0;
+    wifi_second_chan_t second       = 0;
+    mesh_addr_t parent_bssid        = {0};
+    uint8_t sta_mac[MWIFI_ADDR_LEN] = {0};
+    mesh_assoc_t mesh_assoc         = {0x0};
+    wifi_sta_list_t wifi_sta_list   = {0x0};
+
+    esp_wifi_get_mac(ESP_IF_WIFI_STA, sta_mac);
+    esp_wifi_ap_get_sta_list(&wifi_sta_list);
+    esp_wifi_get_channel(&primary, &second);
+    esp_wifi_vnd_mesh_get(&mesh_assoc);
+    esp_mesh_get_parent_bssid(&parent_bssid);
+
+    MDF_LOGI("System information, channel: %d, layer: %d, self mac: " MACSTR ", parent bssid: " MACSTR
+             ", parent rssi: %d, node num: %d, free heap: %u", primary,
+             esp_mesh_get_layer(), MAC2STR(sta_mac), MAC2STR(parent_bssid.addr),
+             mesh_assoc.rssi, esp_mesh_get_total_node_num(), esp_get_free_heap_size());
+
+    for (int i = 0; i < wifi_sta_list.num; i++) {
+        MDF_LOGI("Child mac: " MACSTR, MAC2STR(wifi_sta_list.sta[i].mac));
+    }
+
+    if(config.mesh_type != MESH_ROOT) {
+        MDF_LOGI("This node is resetting, need to update Genre");
+        esp_restart();
+    }
+
+#ifdef MEMORY_DEBUG
+    if (!heap_caps_check_integrity_all(true)) {
+        MDF_LOGE("At least one heap is corrupt");
+    }
+
+    mdf_mem_print_heap();
+    mdf_mem_print_record();
+#endif /**< MEMORY_DEBUG */
+}
+
+
+
+/**
+ * @brief All module events will be sent to this task in esp-mdf
+ *
+ * @Note:
+ *     1. Do not block or lengthy operations in the callback function.
+ *     2. Do not consume a lot of memory in the callback function.
+ *        The task memory of the callback function is only 4KB.
+ */
+static mdf_err_t event_loop_cb(mdf_event_loop_t event, void *ctx)
+{
+    MDF_LOGI("event_loop_cb, event: %d", event);
+
+    switch (event) {
+        case MDF_EVENT_MWIFI_STARTED:
+            MDF_LOGI("MESH is started");
+            break;
+
+        case MDF_EVENT_MWIFI_PARENT_CONNECTED:
+            MDF_LOGI("Parent is connected on station interface");
+            break;
+
+        case MDF_EVENT_MWIFI_PARENT_DISCONNECTED:
+            MDF_LOGI("Parent is disconnected on station interface");
+            break;
+
+        default:
+            break;
+    }
+
+    return MDF_OK;
+}
+
+static mdf_err_t wifi_init()
+{
+    mdf_err_t ret          = nvs_flash_init();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        MDF_ERROR_ASSERT(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    MDF_ERROR_ASSERT(ret);
+
+    tcpip_adapter_init();
+    MDF_ERROR_ASSERT(esp_event_loop_init(NULL, NULL));
+    MDF_ERROR_ASSERT(esp_wifi_init(&cfg));
+    MDF_ERROR_ASSERT(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
+    MDF_ERROR_ASSERT(esp_wifi_set_mode(WIFI_MODE_STA));
+    MDF_ERROR_ASSERT(esp_wifi_set_ps(WIFI_PS_NONE));
+    MDF_ERROR_ASSERT(esp_mesh_set_6m_rate(false));
+    MDF_ERROR_ASSERT(esp_wifi_start());
+
+    return MDF_OK;
+}
+
+void app_main()
+{
+    genre = getGenre(); //must come first due to ADC issus conflicting with wifi
+    
+    mwifi_init_config_t cfg = MWIFI_INIT_CONFIG_DEFAULT();
+    mwifi_config_t config   = {
+        .channel   = CONFIG_MESH_CHANNEL,
+        .mesh_id   = CONFIG_MESH_ID,
+        .mesh_type = CONFIG_DEVICE_TYPE,
+    };
+
+    /**
+     * @brief Set the log level for serial port printing.
+     */
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set(TAG, ESP_LOG_DEBUG);
+    MDF_LOGI("Genre is set to %d", genre);
+
+    /**
+     * @brief Initialize wifi mesh.
+     */
+    MDF_ERROR_ASSERT(mdf_event_loop_init(event_loop_cb));
+    MDF_ERROR_ASSERT(wifi_init());
+    MDF_ERROR_ASSERT(mwifi_init(&cfg));
+    MDF_ERROR_ASSERT(mwifi_set_config(&config));
+    MDF_ERROR_ASSERT(mwifi_start());
+
+    /**
+     * @brief Data transfer between wifi mesh devices
+     */
+    if (config.mesh_type == MESH_ROOT) {
+        xTaskCreate(root_task, "root_task", 4 * 1024,
+                    NULL, CONFIG_MDF_TASK_DEFAULT_PRIOTY, NULL);
+    } else {
+        xTaskCreate(node_write_task, "node_write_task", 4 * 1024,
+                    NULL, CONFIG_MDF_TASK_DEFAULT_PRIOTY, NULL);
+        xTaskCreate(node_read_task, "node_read_task", 4 * 1024,
+                    NULL, CONFIG_MDF_TASK_DEFAULT_PRIOTY, NULL);
+    }
+
+    TimerHandle_t timer = xTimerCreate("print_system_info", 10000 / portTICK_RATE_MS,
+                                       true, NULL, print_system_info_timercb);
+    xTimerStart(timer, 0);
+}
